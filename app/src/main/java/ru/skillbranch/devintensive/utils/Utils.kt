58f3ed3deff30f12cb5c84ea12341c8d5fe02da5
NevzorovPG <nevzorovpg@gmail.com>
2019-07-10 23:11:11 +0700

    hometask_2 complete2

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,10 +2,10 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName : String?) : Pair<String?, String?> {
-        val parts: List<String>? = if (fullName?.split(" ")?.get(0) == "") null else fullName?.split(" ")
+        val parts: List<String>? =  fullName?.split(" ")
 
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        val firstName = parts?.getOrNull(0)?.ifEmpty { null }
+        val lastName = parts?.getOrNull(1)?.ifEmpty { null }
         return firstName to lastName
     }
 
@@ -85,9 +85,9 @@ object Utils {
 
 
     fun toInitials(firstName: String?, lastName: String?): String?{
-        val firstChar: String = firstName?.capitalize()?.getOrNull(0)?.toString()?:""
-        val lastChar: String = lastName?.capitalize()?.getOrNull(0)?.toString()?:""
-        return if(firstChar.plus(lastChar).isBlank()) null else "${firstChar.plus(lastChar)}"
+        val firstN: String = firstName?.trimIndent()?.capitalize()?.getOrNull(0)?.toString() ?: ""
+        val lastN: String = lastName?.trimIndent()?.capitalize()?.getOrNull(0)?.toString() ?: ""
+        return "$firstN$lastN".ifEmpty { null }
     }
 }
 
